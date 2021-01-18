9c05bdb325f7a77dd04fadb71bc2d93d75e7e4be
Matias <amb19821982@gmail.com>
2021-01-17 21:45:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var indexValue = 0;
+      function slideShow(){
+        setTimeout(slideShow, 4000);
+        var x;
+        const img = document.querySelectorAll("img");
+        for(x = 0; x < img.length; x++){
+          img[x].style.display = "none";
+        }
+        indexValue++;
+        if(indexValue > img.length){indexValue = 1}
+        img[indexValue -1].style.display = "block";
+      }
+      slideShow();
+      
